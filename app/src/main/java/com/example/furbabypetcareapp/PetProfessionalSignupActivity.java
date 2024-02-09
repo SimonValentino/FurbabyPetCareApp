766f585771f17b81a3af9b59a3c41ebd97add507
marriojeyakumar <107595256+marriojeyakumar@users.noreply.github.com>
2024-02-09 00:04:03 -0500

    Finished skeleton/general landscape of pet professional signup page. Still need to check to make sure the neccecary text boxes are filled.

--- app/src/main/java/com/example/furbabypetcareapp/PetProfessionalSignupActivity.java
+++ app/src/main/java/com/example/furbabypetcareapp/PetProfessionalSignupActivity.java
@@ -1,6 +1,8 @@
 package com.example.furbabypetcareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class PetProfessionalSignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_professional_signup);
+    }
+    public void submitForm(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
     }
 }
