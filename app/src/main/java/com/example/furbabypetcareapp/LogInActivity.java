c2ba8586cd95de87f95de98dd8e64c60326c9099
marriojeyakumar <107595256+marriojeyakumar@users.noreply.github.com>
2024-03-07 23:53:28 -0500

    Database signup is finally working.

--- app/src/main/java/com/example/furbabypetcareapp/LogInActivity.java
+++ app/src/main/java/com/example/furbabypetcareapp/LogInActivity.java
@@ -3,14 +3,43 @@ package com.example.furbabypetcareapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LogInActivity extends AppCompatActivity {
 
+    EditText emailEditText;
+    EditText passwordEditText;
+    String email, password, apiKey;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+
+        if(sharedPreferences.getString("logged", "false").equals("true")) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
@@ -20,7 +49,52 @@ public class LogInActivity extends AppCompatActivity {
     }
 
     public void logIn(View view) {
-        alert("Code for logging in");
+        String emailEdit = emailEditText.getText().toString();
+        String passwordEdit = passwordEditText.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url ="http://192.168.1.191/login-registration-android/login.php";
+
+        Log.i("continueSignup", "continueSignup");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String status = jsonObject.getString("status");
+                            String message = jsonObject.getString("message");
+                            if (status.equals("success")) {
+                                email = jsonObject.getString("email");
+                                apiKey = jsonObject.getString("apiKey");
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("logged", "true");
+                                editor.putString("email", email);
+                                editor.putString("apiKey", apiKey);
+                                editor.apply();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Submission failed", "Submission failed");
+                Log.i("adada", error.toString());
+            }
+        }){
+            protected Map<String, String> getParams(){
+                Map<String, String> paramV = new HashMap<>();
+                paramV.put("email", emailEdit);
+                paramV.put("password", passwordEdit);
+                return paramV;
+            }
+        };
+        queue.add(stringRequest);
     }
 
     public void goToSignup(View view) {
